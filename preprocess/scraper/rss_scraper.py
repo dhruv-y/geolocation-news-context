9ff860b5cf56b66c61ce79bf3353b7f561131cca
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2021-03-31 19:57:45 -0400

    initial project setup

---
+++ preprocess/scraper/rss_scraper.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+'''
+1. define scraper function to access RSS feed list
+2. scrape the following for each article - link, desc, content
+3. create df to insert into columns
+'''
