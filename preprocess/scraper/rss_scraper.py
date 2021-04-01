79c11b1671c56f229921befd30aec8a118c1fc46
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2021-03-31 23:41:31 -0400

    add initial scraper files

--- preprocess/scraper/rss_scraper.py
+++ preprocess/scraper/rss_scraper.py
@@ -1,8 +1,34 @@
-import requests
+import os, json, requests
+import pandas as pd
 from bs4 import BeautifulSoup
+from urls import url_links
 
 '''
 1. define scraper function to access RSS feed list
 2. scrape the following for each article - link, desc, content
 3. create df to insert into columns
 '''
+
+def article_content(urls):
+    try:
+        for url in urls:
+
+            # Get page content
+            article = requests.get(url)
+
+            # 1. 
+            soup = BeautifulSoup(article.content, features='xml')
+
+            print(url)
+            print('Scraping job success: ', article.status_code)
+        return
+
+    except Exception as e:
+        print("Scraping job failed! Check exception: ")
+        print(e)
+
+
+if __name__ == '__main__':
+    print('Started Scrape')
+    article_content(url_links)
+    print('Finished Scrape')
---
+++ preprocess/scraper/urls.py
@@ -0,0 +1,15 @@
+url_links = [
+        "https://www.southbendtribune.com/search/?f=rss&amp",
+        "https://www.heraldbulletin.com/search/?f=rss&amp",
+        "https://www.nwitimes.com/search/?f=rss&amp",
+        "https://www.kokomotribune.com/search/?f=rss&amp",
+        "https://www.newsandtribune.com/search/?f=rss&amp",
+        "https://www.tribstar.com/search/?f=rss&amp",
+        "https://www.washtimesherald.com/search/?f=rss&amp",
+        "https://www.elkharttruth.com/search/?f=rss&amp",
+        "https://www.goshennews.com/search/?f=rss&amp",
+        "https://www.greensburgdailynews.com/search/?f=rss&amp",
+        "https://www.reporter.net/search/?f=rss&amp",
+        "https://www.pharostribune.com/search/?f=rss&amp",
+        "https://ss-times.com/feed/"
+    ]
